3f68a55dd3b04390864f80771d737227844662b1
Ignacio Vega <nachovega@Ignacios-MacBook-Air.local>
2023-11-29 22:54:39 -0300

    feat: added: authentication to app.js

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 import express from "express";
 import pkg from "dotenv";
 const { dotenv } = pkg;
-
 const result = pkg.config();
+
+import { authRouter } from "./src/routes/index.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
 const defaultEndpoint = "/api";
 
 app.use(express.json());
+
+app.use(`${defaultEndpoint}/auth`, authRouter);
 
 app.get("/", (req, res) => {
   res.status(200).json({
