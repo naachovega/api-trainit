3d3ec3003cf566507b20c44adb7379fbed6ae286
Ignacio Vega <nachovega@Ignacios-MacBook-Air.local>
2023-11-30 18:24:27 -0300

    feature: added hashing helper function

---
+++ src/Helpers/password-helper.js
@@ -0,0 +1,23 @@
+const { randomBytes, pbkdf2Sync } = await import("node:crypto");
+
+function hashPassword(password) {
+  const salt = randomBytes(16).toString("hex");
+
+  const hash = pbkdf2Sync(
+    password.toString(),
+    salt,
+    1000,
+    64,
+    `sha512`
+  ).toString(`hex`);
+
+  console.log(hash, salt);
+
+  const returnObject = {
+    hash: hash,
+    salt: salt,
+  };
+  return returnObject;
+}
+
+export { hashPassword };
