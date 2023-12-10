81d6b4866cfd8477c696fa4b65a8393c8ab3c1b8
Ignacio Vega <nachovega@Ignacios-MacBook-Air.local>
2023-12-09 23:46:05 -0300

    feature: added error middlewares

--- app.js
+++ app.js
@@ -1,24 +1,36 @@
 import express from "express";
 import pkg from "dotenv";
+import { authRouter } from "./src/routes/index.js";
+import {
+  errorMiddelware,
+  apiKeyMiddleware,
+  notFoundMiddlware,
+} from "./src/Middleware/index.js";
+
 const { dotenv } = pkg;
 const result = pkg.config();
-
-import { authRouter } from "./src/routes/index.js";
-
 const app = express();
 const port = process.env.PORT || 3000;
 const defaultEndpoint = "/api";
 
 app.use(express.json());
 
-app.use(`${defaultEndpoint}/auth`, authRouter);
+app.use(`${defaultEndpoint}/auth`, apiKeyMiddleware, authRouter);
 
 app.get("/", (req, res) => {
-  res.status(200).json({
+  let startTime = performance.now();
+  let endTime = performance.now();
+  let timeElapsed = endTime - startTime;
+
+  return res.status(200).json({
     status: "API currently running",
+    timestamp: timeElapsed,
   });
 });
 
+app.use(notFoundMiddlware);
+app.use(errorMiddelware);
+
 app.listen(port, () => {
-  console.log(`API Listening on port: ${port}`);
+  console.log(`API Running and listening on port: ${port}`);
 });
