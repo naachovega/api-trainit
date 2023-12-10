cdc92574013f33d637495272d3d386ebd9cfc2bf
Ignacio Vega <nachovega@Ignacios-MacBook-Air.local>
2023-12-09 22:08:13 -0300

    added the finish registration endpoint

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,50 +1,79 @@
 import express from "express";
-import { createUserByEmail, getUsersCredential } from "../Controller/index.js";
+import {
+  createUserByEmail,
+  getUsersCredential,
+  finishRegister,
+} from "../Controller/index.js";
 import { CustomError } from "../Models/Interfaces/Errors.js";
-
+import { userRepository } from "../Repository/index.js";
 const authRouter = express.Router();
 
 authRouter.post("/register", async (req, res) => {
   try {
-
     const { email, password } = req.body;
 
     const { user, getUserErr } = await getUsersCredential(email);
 
     if (getUserErr) {
-      throw new CustomError(err.message, err.code, err.stackErrorMessage)
+      throw new CustomError(err.message, err.code, err.stackErrorMessage);
     }
 
     if (user.length > 0) {
-      return res.json({
-        message: "User already exists.",
-        code: 409,
-      }).status(409);
+      return res
+        .json({
+          message: "User already exists.",
+          code: 409,
+        })
+        .status(409);
     }
 
     let createErr = await createUserByEmail(email, password);
 
     if (createErr) {
-      throw new CustomError(err.message, err.code, err.stackErrorMessage)
+      throw new CustomError(err.message, err.code, err.stackErrorMessage);
     }
 
-    return res.json({
-      message: "The user was created OK",
-      code: 201,
-    }).status(201);
-
+    return res
+      .json({
+        message: "The user was created OK",
+        code: 201,
+      })
+      .status(201);
   } catch (err) {
-
-    return res.json({
-      message: err.message,
-      code: err.code,
-      stackErrorMessage: err.stackErrorMessage,
-    }).status(err.code);
+    return res
+      .json({
+        message: err.message,
+        code: err.code,
+        stackErrorMessage: err.stackErrorMessage,
+      })
+      .status(err.code);
   }
 });
 
 authRouter.get("/sign-in", (req, res) => {
-  const { email, password } = req.body
+  const { email, password } = req.body;
+});
+
+//add middleware to check if the user exists
+//add middleware to check if the user has already finish registering
+authRouter.patch("/finish-register", async (req, res) => {
+  const err = await finishRegister(req);
+
+  if (err) {
+    return res
+      .json({
+        message: err.message,
+        code: err.code,
+      })
+      .status(err.code);
+  }
+
+  res
+    .json({
+      message: "The user information was updated correctly",
+      code: 200,
+    })
+    .status(200);
 });
 
 export default authRouter;
