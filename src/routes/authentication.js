cf4fccd4065326eee7ec5e9485cd294e47e2d91a
Ignacio Vega <nachovega@Ignacios-MacBook-Air.local>
2023-12-10 22:12:03 -0300

    feature: added sign in endpoint

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,12 +1,18 @@
 import express from "express";
-import { createUserByEmail, finishRegister } from "../Controller/index.js";
+import {
+  createUserByEmail,
+  finishRegister,
+  signIn,
+} from "../Controller/index.js";
 import { CustomError } from "../Models/Interfaces/Errors.js";
 import {
-  userExistMiddleware,
+  userExistByEmailMiddleware,
+  userExistByIdMiddleware,
   userDoesNotExistMiddleware,
   alreadyRegisteredMiddleware,
   validateEmailMiddleware,
   validatePasswordMiddleware,
+  validateSignInMiddleware,
 } from "../Middleware/index.js";
 
 const authRouter = express.Router();
@@ -44,13 +50,35 @@ authRouter.post(
   }
 );
 
-authRouter.get("/sign-in", (req, res) => {
-  const { email, password } = req.body;
-});
+authRouter.post(
+  "/sign-in",
+  userExistByEmailMiddleware,
+  validateSignInMiddleware,
+  async (req, res) => {
+    const { email, password } = req.body;
+    const { user, err } = await signIn(email, password);
+
+    if (err) {
+      return res
+        .json({
+          message: err.message,
+          code: err.code,
+        })
+        .status(err.code);
+    }
+
+    return res
+      .json({
+        user: user[0],
+        code: 200,
+      })
+      .status(200);
+  }
+);
 
 authRouter.patch(
   "/finish-register",
-  userExistMiddleware,
+  userExistByIdMiddleware,
   alreadyRegisteredMiddleware,
   async (req, res) => {
     const err = await finishRegister(req);
