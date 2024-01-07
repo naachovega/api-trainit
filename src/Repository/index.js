cf71998690b678aeb71644804c24752be23549e8
naachovega <igna.vega@hotmail.com>
2024-01-07 17:41:24 -0300

    feat: added repository logic to insert and read routines

--- src/Repository/index.js
+++ src/Repository/index.js
@@ -1,7 +1,9 @@
 import { AuthenticationRepository } from "./authentication-repository.js";
 import { UserRepository } from "./user-repository.js";
+import { RoutineRepository } from "./routine-repository.js";
 
 const authRepository = new AuthenticationRepository();
 const userRepository = new UserRepository();
+const routineRepository = new RoutineRepository();
 
-export { authRepository, userRepository };
+export { authRepository, userRepository,routineRepository };
--- src/Repository/routine-repository.js
+++ src/Repository/routine-repository.js
@@ -4,4 +4,16 @@ export class RoutineRepository {
   constructor() {
     this.storage = routineStorage;
   }
+
+  async create(routine) {
+    return await this.storage.create(routine);
+  }
+
+  async findAll() {
+    return await this.storage.getAll();
+  }
+
+  async find(id) {
+    return await this.storage.get(id);
+  }
 }
