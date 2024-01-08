27f1c2c3a30643832f800327c41ec21b9a8cc9b4
naachovega <igna.vega@hotmail.com>
2024-01-07 22:25:10 -0300

    feat: added endpoint to get routines by user id

--- src/routes/routine.js
+++ src/routes/routine.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { Create, GetAll, Get } from "../Controller/index.js";
+import { Create, GetAll, Get, GetByUserId } from "../Controller/index.js";
 import { userExistByIdMiddleware } from "../Middleware/user-middleware.js";
 import Routine from "../Models/routine.js";
 
@@ -65,6 +65,28 @@ routineRouter.get("/:id", async (req, res) => {
 
   return res.status(200).json({
     data: routine,
+  });
+});
+
+routineRouter.get("/byUserId/:userId", async (req, res) => {
+  const { userId } = req.params;
+
+  const { routines, err } = await GetByUserId(userId);
+  
+  if (err) {
+    return res.status(err.code).json({
+      message: err.message,
+    });
+  } 
+
+  if (routines.length === 0) {
+    return res.status(404).json({
+      data: "no routines found for the user sent",
+    });
+  }
+
+  return res.status(200).json({
+    data: routines,
   });
 });
 
