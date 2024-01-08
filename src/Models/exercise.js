6f5d3b06a18d6f1ab4fb4a85e9c9bb0f6215e530
naachovega <igna.vega@hotmail.com>
2024-01-08 19:18:10 -0300

    feat: modified the structure of the exercise

--- src/Models/exercise.js
+++ src/Models/exercise.js
@@ -1,15 +1,26 @@
 import { v4 as uuid } from "uuid";
 
 export default class Exercise {
-  constructor(name, description, desiredReps, desiredWeight, routineId) {
+  constructor(
+    name,
+    description,
+    desiredReps,
+    desiredSet,
+    desiredWeight,
+    restTime,
+    routineId
+  ) {
     this._id = uuid();
     this.name = name;
     this.description = description;
     this.desiredReps = desiredReps;
-    this.actualReps = 0.0;
+    this.actualReps = 0;
+    this.desiredSet = desiredSet;
+    this.actualSet = 0;
     this.desiredWeight = desiredWeight;
-    this.routineId = routineId;
     this.actualWeight = 0.0;
+    this.restTime = restTime;
+    this.routineId = routineId;
     this.completed = false;
   }
 }
